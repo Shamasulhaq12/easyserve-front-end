5e70445b681cce156a01baa35efc9a7c56036999
abdussamud <abdulsamad.aaa123@gmail.com>
2025-12-06 13:16:22 +0500

    remove .next

--- .next/static/chunks/src_app_admin_layout_jsx_1935eda3._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_08f823df._.js",
-  "static/chunks/src_c6c05195._.js"
-],
-    source: "dynamic"
-});
--- .next/static/chunks/src_app_auth_verify_page_jsx_cc839114._.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/src_app_auth_verify_page_jsx_4ffbbd99._.js"
-],
-    source: "dynamic"
-});
